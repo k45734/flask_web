2d816bd9506f4bf6bf357d501c10e62120da8280
k45734 <71308262+k45734@users.noreply.github.com>
2021-06-25 09:03:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,26 +36,9 @@ def create_app():
 	filepath = './log/flask.log'
 	if not os.path.isfile(filepath):
 		f = open('./log/flask.log','a', encoding='utf-8')
-	#logger = logging.getLogger(__name__)
-	#logging.basicConfig(format = '%(asctime)s:%(levelname)s:%(message)s', 
-	#					datefmt = '%m/%d/%Y %I:%M:%S %p', 
-	#					filename = "./log/flask.log", 
-	#					encoding='utf-8',
-	#					level = logging.DEBUG
-	#					)
-	log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
-	logFile = './log/flask.log'
-	my_handler = RotatingFileHandler(logFile, 
-									mode='a', 
-									maxBytes=5*1024*1024, 
-									backupCount=2, 
-									encoding='utf-8', 
-									delay=0)
-	my_handler.setFormatter(log_formatter)
-	my_handler.setLevel(logging.DEBUG)
-	app_log = logging.getLogger(__name__)
-	app_log.setLevel(logging.DEBUG)
-	app_log.addHandler(my_handler)
+	rfh = logging.handlers.RotatingFileHandler(filename='./log/flask.log', mode='a', maxBytes=5*1024*1024, backupCount=2, encoding=None, delay=0)
+	logging.basicConfig(level=logging.DEBUG,format="%(asctime)s %(name)-25s %(levelname)-8s %(message)s",datefmt="%y-%m-%d %H:%M:%S",handlers=[rfh])
+	logger = logging.getLogger(__main__)
 	app = Flask(__name__)	
 	app.secret_key = os.urandom(12)
 	from pages import main_page
