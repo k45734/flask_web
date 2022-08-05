4582a19da8211687b1d35c3cf9d38fb3f92ccd7d
k45734 <71308262+k45734@users.noreply.github.com>
2022-08-05 21:37:44 +0900

    Update sub2_page.py

--- pages/sub2_page.py
+++ pages/sub2_page.py
@@ -839,7 +839,7 @@ def ali(telgm,telgm_alim,telgm_token,telgm_botid,newdate,a4):
 		e = row['COMPLETE']
 		msg = '{}\n{}\n{}'.format(a,b,d)
 		tel(telgm,telgm_alim,telgm_token,telgm_botid,msg)
-		#time.sleep(10)
+		time.sleep(10)
 		#중복 알림에거
 		addnews_d(a,b,c,d,e,newdate)
 			
