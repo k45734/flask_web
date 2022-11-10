2c21a848b786fc2a8670d833a3feb3488397cf70
k45734 <71308262+k45734@users.noreply.github.com>
2022-11-10 12:44:01 +0900

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -76,7 +76,8 @@ except:
 	pass
 if not os.path.isfile(filepath):
 	f = open(logdata + '/flask.log','a', encoding='utf-8')
-rfh = logging.handlers.RotatingFileHandler(filename=logdata + '/flask.log', mode='a', maxBytes=5*1024*1024, backupCount=0, encoding='utf-8', delay=0)
+fileMaxByte = 1024*50
+rfh = logging.handlers.RotatingFileHandler(filename=logdata + '/flask.log', mode='a', maxBytes=fileMaxByte, backupCount=5, encoding='utf-8', delay=0)
 logging.basicConfig(level=logging.INFO,format="[%(asctime)s %(filename)s:%(lineno)d %(levelname)s] - %(message)s",datefmt='%Y-%m-%d %H:%M:%S',handlers=[rfh])
 logger = logging.getLogger()
 logging.getLogger('apscheduler.executors.default').setLevel(logging.WARNING)
